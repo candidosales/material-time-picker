b15984e1eae837b26f89fa347fa252caf52b8216
Candido Sales Gomes <candidosg@gmail.com>
2026-02-16 15:51:21 -0500

    Remove unused provideAnimations import from main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { enableProdMode, importProvidersFrom, provideZoneChangeDetection } from 
 import { environment } from "./environments/environment";
 import { AppComponent } from "./app/app.component";
 import { MatCardModule } from "@angular/material/card";
-import { provideAnimations } from "@angular/platform-browser/animations";
 import { BrowserModule, bootstrapApplication } from "@angular/platform-browser";
 
 if (environment.production) {
@@ -13,6 +12,5 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     provideZoneChangeDetection(),importProvidersFrom(BrowserModule, MatCardModule),
-    provideAnimations(),
   ],
 }).catch((err) => console.error(err));
